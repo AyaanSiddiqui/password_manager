b523b49f419e45d3e7121d97ae15656ad65e727f
Ayaan <sidayaan753@gmail.com>
2022-02-11 09:00:23 +0530

    Adding password cracking facility

--- menu.py
+++ menu.py
@@ -1,13 +1,28 @@
-import test
+from connectingsql import inserting, displaying,displaying_websites,create_table
 
 def menu():
     print('-'*30)
     print(('-'*13) + 'Menu'+ ('-' *13))
-    print('1. Create new password')
-    print('2. Find all sites and apps connected to an email')
-    print('3. Find a password for a site or app')
+    print('1. Save new password')
+    print('2. Find a password for a site or app')
+    print("3. Display all apps and websites")
+    print('4. Create table if not present')
     print('Q. Exit')
     print('-'*30)
-    return input(': ')
-menu()
+    return input(": ")
 
+choice=menu()
+
+while choice!="Q":
+    if choice=="1":
+        inserting()
+    if choice=="2":
+        displaying()
+    if choice=="3":
+        displaying_websites()
+    if choice=="4":
+        create_table()
+    if choice!="1" or choice!="2" or choice!="3" or choice!="4":
+        choice=menu()
+        
+exit()
